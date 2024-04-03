96dea89417e5123cc7387a54a716213d0995e28d
canfieldben <71992055+canfieldben@users.noreply.github.com>
2024-04-03 12:39:24 -0400

    adding form framework

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class StockForm(FlaskForm):
+    stock = StringField('Stock Code', validators=[DataRequired])
+    submit = SubmitField('Run Analysis')
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,11 @@ from src import *
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/display')
+def display():
     stock_request.stock_request("AAPL")
     reddit_requests.reddit_request("AAPL")
     return render_template('plot.html')
